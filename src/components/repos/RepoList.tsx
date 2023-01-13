a413b39b3f8bc2d5dbf28a80a9486133f8aa4c58
Ajay Sinha <ajaysinhaa09@gmail.com>
2023-01-13 19:19:34 +0530

    final commit

--- src/components/repos/RepoList.tsx
+++ src/components/repos/RepoList.tsx
@@ -4,8 +4,8 @@ type Props = {
 }
 function RepoList({ repos }: Props): JSX.Element {
   return (
-    <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats ">
-      <div className="card-normal lg:card-body  md:card-body ">
+    <div className="w-full py-6  rounded-lg shadow-lg card bg-base-100">
+      <div className="card-normal lg:card-body  md:card-body">
         <h2 className="text-3xl my-4 font-bold card-title">
           Latest Repositories
         </h2>
