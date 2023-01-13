ee2f875946da5d203d8cf9808f1b53c942680a2e
Ajay Sinha <ajaysinhaa09@gmail.com>
2023-01-13 18:45:19 +0530

    searchbar position setting

--- src/components/users/UserSearch.tsx
+++ src/components/users/UserSearch.tsx
@@ -32,7 +32,7 @@ function UserSearch(): JSX.Element {
     }
   }
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-24 gap-8">
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-16 gap-8">
       <div>
         <form onSubmit={handleSubmit}>
           <div className="form-control">
