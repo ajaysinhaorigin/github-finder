201671d80c42ca1f91d8f28658918a012e1ba5c0
Ajay Sinha <ajaysinhaa09@gmail.com>
2023-01-13 14:23:49 +0530

    remve readme file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
